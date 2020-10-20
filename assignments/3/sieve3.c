9df4c5d72f687215112c7f0bf12da8b027e39802
Casey Key <caseykey@rams.colostate.edu>
2020-10-20 10:43:40 -0600

    testing sieve3

--- assignments/3/sieve3.c
+++ assignments/3/sieve3.c
@@ -42,21 +42,10 @@ int main(int argc, char **argv) {
    if ( argc > 1 ) N  = atoi(argv[1]);
    if ( argc > 2 ) BLKSIZE = atoi(argv[2]);
 
-   // N will get cut in half to save memory
-   long problem_size = N;
-
    /* Start Timer */
 
    initialize_timer ();
    start_timer();
-
-   // Only store odds
-   // Half memory if N % 2 == 0
-   // Half memory +1 if N is odd
-   if (N % 2 == 0)
-       N = (N/2);
-   else
-       N = (N/2 + 1);
 
    // +1 for null character 
    size = (N+1)*sizeof(char);
@@ -69,53 +58,42 @@ int main(int argc, char **argv) {
    // We want to skip the value 1 (results start from 2)
    mark[0] = 1;
 
-   long sqrt_p_size = sqrt(problem_size);
-   for (long k = 3; k <= sqrt_p_size; k += 2) { // Iterate to sqrt(n)
+   long sqrt_N = sqrt(N);
+   for (long k = 3; k <= sqrt_N; k += 2) { // Iterate to sqrt(n)
         // We only want to find primes less than sqrt(N)
         // We stride by 2*k because odd + odd = even
         // And odd + even = odd, thereby skipping all even multiples
         // All primes are odd aside from 2
         #pragma omp parallel for
-        for (long prime = k*k; prime <= sqrt_p_size; prime += 2*k) mark[prime/2] = 1;
+        for (long prime = k*k; prime <= sqrt_N; prime += 2*k) mark[prime] = 1;
         
         // Get next odd prime (unmarked value)
-        while (mark[k/2]) { k += 2; count++; }
+        while (mark[k]) k += 2;
    }
 
 
    /*number of primes*/
-   long primes[sqrt_p_size]; // (int *) malloc(count * sizeof(int));
+   long primes[sqrt_N]; // (int *) malloc(count * sizeof(int));
    primes[0] = 2; // Our algorithm doesn't include 2 because it's even
    
    // Count starts from 1 to include 2
    // i starts from 3 so that we only count odds
    count = 1;
-   for(i = 3; i <= sqrt_p_size; i+=2){
-        if(mark[i/2] == 0) {
-        	printf("\t prime %ld, index %ld \n", i, i/2);
+   for(i = 3; i <= sqrt_N; i+=2){
+        if(mark[i] == 0) {
             count++;
             primes[i/2] = i;
         }
    }
 
-   printf("There are %ld primes for sqrt(N).\n", count);
-   printf("The primes:\n");
-   for(int q = 0; q < count; q++) printf("%ld\n", primes[q]);
-
    /* end of preamble */
-
-   // +1 for null character 
-   size = (problem_size+1)*sizeof(char);
-   marked = (char *)realloc(mark, size);
 
    int prime;
    for (int j = 0; j < count; j++) {
        prime = primes[j];
-       for (int ii = sqrt_p_size; ii < problem_size; ii += BLKSIZE) {
-           printf("ii: %d\n", ii);
-           for (int i = FMIB(ii, prime); i <= minn(ii+BLKSIZE, problem_size); i += prime) {
-               printf("FMIB(%d, %d)=%d, i %d, <= %d\n", ii, prime, FMIB(ii, prime), i, minn(ii+BLKSIZE, problem_size)); 
-               marked[i] = 1;
+       for (int ii = sqrt_N; ii < N; ii += BLKSIZE) {
+           for (int i = FMIB(ii, prime); i <= minn(ii+BLKSIZE, N); i += prime) {
+               mark[i] = 1;
            }
        }
    } 
@@ -127,21 +105,18 @@ int main(int argc, char **argv) {
 
    /*number of primes*/
    // Count and i are magic numbers
-   count = 0;
-   for(i = 2; i <= problem_size; i+=1){
-        if(marked[i] == 0) {
-        	 printf("\t prime %ld  \n",i );
-        	++count;
-        }
-
+   count = 1;
+   for(i = 3; i <= N; i+=2){
+        if(mark[i] == 0) ++count;
    }
-   printf("There are %ld primes less than or equal to %ld\n", count, problem_size);
+
+   printf("There are %ld primes less than or equal to %ld\n", count, N);
    /* print results */
    printf("First three primes:");
    j = 1;
    printf("%d ", 2);
-   for ( i=3 ; i <= problem_size ; i+=2 ) {
-      if (marked[i]==0){
+   for ( i=3 ; i <= N && j < 3; i+=2 ) {
+      if (mark[i]==0){
             printf("%ld ", i);
             ++j;
       }
@@ -150,9 +125,9 @@ int main(int argc, char **argv) {
 
    printf("Last three primes:");
    j = 0;
-   n=(problem_size%2?problem_size:problem_size-1);
+   n=(N%2?N:N-1);
    for (i = n; i > 1 && j < 3; i-=2){
-     if (marked[i]==0){
+     if (mark[i]==0){
         printf("%ld ", i);
         j++;
      }
---
+++ assignments/utils/collect_sieve3_data.py
@@ -0,0 +1,180 @@
+#!/usr/bin/python3
+
+import getopt, sys
+import re
+import os
+import subprocess
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+time_token: 'time ='
+
+sieve1 = {
+    'program_name': './sieve1',
+}
+
+sieve2 = {
+    'program_name': './sieve2-1',
+    'time_token': 'time ='
+}
+
+sieve3 = {
+    'program_name': './sieve3',
+    'arguments': '10',
+    'time_token': 'time ='
+}
+
+
+def plot_multi(data, cols=None, spacing=.1, **kwargs):
+
+    from pandas import plotting
+
+    # Get default color style from pandas - can be changed to any other color list
+    if cols is None: cols = data.columns
+    if len(cols) == 0: return
+    colors = ['red', 'green', 'blue']
+
+    # First axis
+    ax = data.loc[:, cols[0]].plot(kind='scatter', label=cols[0], color=colors[0], **kwargs)
+    ax.set_ylabel(ylabel=cols[0])
+    lines, labels = ax.get_legend_handles_labels()
+
+    for n in range(1, len(cols)):
+        # Multiple y-axes
+        ax_new = ax.twinx()
+        ax_new.spines['right'].set_position(('axes', 1 + spacing * (n - 1)))
+        data.loc[:, cols[n]].plot(kind='scatter', ax=ax_new, label=cols[n], color=colors[n % len(colors)], **kwargs)
+        ax_new.set_ylabel(ylabel=cols[n])
+        ax_new.set_ylim([0, data.loc[:, cols[n]].max()])
+
+        # Proper legend position
+        line, label = ax_new.get_legend_handles_labels()
+        lines += line
+        labels += label
+
+    ax.legend(lines, labels, loc=0)
+    ax.set_xlabel("Iteration")
+    return ax
+
+# [program] Dict
+def collect_data(program, sizes):
+    times = []
+    thread_means = []
+    for threads in range(1, 9):
+        print("Threads:", threads)
+        os.environ['OMP_NUM_THREADS'] = str(threads)
+        for size in sizes:
+            results = []
+            for i in range(0, 8): 
+                output = subprocess.run([program['program_name']] + str(size).split(), capture_output=True).stdout.decode('utf-8')
+                result = re.search('(?<=' + program['time_token'] + ').\S*', output).group(0).strip()
+                print((threads, size, result))
+                results.append(float(result))
+            results.remove(max(results))
+            results.remove(min(results))
+            mean = sum(results)/len(results)
+            thread_means.append((threads, size, mean))
+    return thread_means 
+
+# [program] Dict
+def collect_seq_data(program, sizes):
+    times = []
+    problem_size_means = []
+    for problem_size in sizes:
+        print("Problem size:", problem_size)
+
+        results = []
+        for i in range(0, 8): 
+            output = subprocess.run([program['program_name']] + str(problem_size).split(), capture_output=True).stdout.decode('utf-8')
+            result = re.search('(?<=' + program['time_token'] + ').\S*', output).group(0).strip()
+            print(result)
+            results.append(float(result))
+        results.remove(max(results))
+        results.remove(min(results))
+        mean = sum(results)/len(results)
+        problem_size_means.append(mean)
+    return problem_size_means 
+
+
+# index represents the index of sizes array
+def calculate_speedup(baseline_mean, test_times):
+    speedup_list = []
+    # datum = (threads, problem_size, mean_time)
+    for datum in test_times:
+        speedup = baseline_mean / datum[2] * 100
+        speedup_list.append((datum[0], datum[1], speedup))
+    return speedup_list
+
+
+#sizes = [500000 , 1000000, 1500000]
+sizes = [500000000, 1000000000, 1500000000] #500mil, 1bil, 1.5bil
+
+sieve1_results = collect_seq_data(sieve1, sizes)
+baseline_mean = sum(sieve1_results)/len(sieve1_results)
+
+sieve2_results = collect_data(sieve2, sizes)
+sieve2_speedups = calculate_speedup(baseline_mean, sieve2_results)
+
+sieve2_1_results = collect_data(sieve2_1, sizes)
+sieve2_1_speedups = calculate_speedup(baseline_mean, sieve2_1_results)
+
+sieve2_2_results = collect_data(sieve2_2, sizes)
+sieve2_2_speedups = calculate_speedup(baseline_mean, sieve2_2_results)
+
+
+print(sieve2_speedups[0])
+data = pd.DataFrame({
+    "Threads": [i for i in range(1, 9)],
+    "0.5B 2-1": [speedup[2] for speedup in sieve2_speedups if speedup[1] == sizes[0]], 
+    "1.0B 2-1": [speedup[2] for speedup in sieve2_speedups if speedup[1] == sizes[1]],
+    "1.5B 2-1": [speedup[2] for speedup in sieve2_speedups if speedup[1] == sizes[2]],
+    "0.5B 2-2": [speedup[2] for speedup in sieve2_1_speedups if speedup[1] == sizes[0]], 
+    "1.0B 2-2": [speedup[2] for speedup in sieve2_1_speedups if speedup[1] == sizes[1]],
+    "1.5B 2-2": [speedup[2] for speedup in sieve2_1_speedups if speedup[1] == sizes[2]],
+    "0.5B 2-3": [speedup[2] for speedup in sieve2_2_speedups if speedup[1] == sizes[0]],
+    "1.0B 2-3": [speedup[2] for speedup in sieve2_2_speedups if speedup[1] == sizes[1]],
+    "1.5B 2-3": [speedup[2] for speedup in sieve2_2_speedups if speedup[1] == sizes[2]]
+
+})
+
+
+filename = 'sieve2'
+data.to_csv(filename + '.csv', index = False)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.title('sieve2 0.5B Statistics')
+ax.scatter(data['Threads'], data['0.5B 2-1'], label = '2-1')
+ax.scatter(data['Threads'], data['0.5B 2-2'], label = '2-2')
+ax.scatter(data['Threads'], data['0.5B 2-3'], label = '2-3')
+plt.xlabel('Threads')
+plt.ylabel('Speedup (%)')
+plt.legend()
+plt.savefig('sieve2-test1.png')
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.title('sieve2 1.0B Statistics')
+ax.scatter(data['Threads'], data['1.0B 2-1'], label = '2-1')
+ax.scatter(data['Threads'], data['1.0B 2-2'], label = '2-2')
+ax.scatter(data['Threads'], data['1.0B 2-3'], label = '2-3')
+plt.xlabel('Threads')
+plt.ylabel('Speedup (%)')
+plt.legend()
+plt.savefig('sieve2-test2.png')
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.title('sieve2 1.5B Statistics')
+ax.scatter(data['Threads'], data['1.5B 2-1'], label = '2-1')
+ax.scatter(data['Threads'], data['1.5B 2-2'], label = '2-2')
+ax.scatter(data['Threads'], data['1.5B 2-3'], label = '2-3')
+plt.xlabel('Threads')
+plt.ylabel('Speedup (%)')
+plt.legend()
+plt.savefig('sieve2-test3.png')
+
+#plt.show()    
+
