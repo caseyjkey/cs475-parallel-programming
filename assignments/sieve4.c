5d812562b90a16364c67cce763b6aebf4ae37296
Casey Key <caseykey@rams.colostate.edu>
2020-10-28 20:56:55 -0600

    added remote work

---
+++ assignments/sieve4.c
@@ -0,0 +1,144 @@
+/*/////////////////////////////////////////////////////////////////////////////
+//
+// File name : sieve.c
+// Author    : Nissa Osheim
+// Date      : 2010/19/10
+// Desc      : Finds the primes up to N
+//
+// updated Wim Bohm
+/////////////////////////////////////////////////////////////////////////////*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "timer.h"
+
+int main(int argc, char **argv) {
+
+   long N  = 100;
+
+   char *mark;
+
+   long   size;
+   long   curr;
+   long   i, j,n;
+   long   count;
+
+   /* Time */
+
+   double time;
+
+   if ( argc > 1 ) N  = atoi(argv[1]);
+
+   // N will get cut in half to save memory
+   long problem_size = N;
+
+   /* Start Timer */
+
+   initialize_timer ();
+   start_timer();
+
+   // Only store odds
+   // Half memory if N % 2 == 0
+   // Half memory +1 if N is odd
+   if (N % 2 == 0)
+       N = (N/2);
+   else
+       N = (N/2 + 1);
+
+   // +1 for null character 
+   size = (N+1)*sizeof(char);
+   mark = (char *)malloc(size);
+
+   for (i=1; i<=N; i=i+1){
+     mark[i]=0;
+   }
+
+   // We want to skip the value 1 (results start from 2)
+   mark[0] = 1;
+
+   count = 1;
+   long sqrt_p_size = sqrt(problem_size);
+   for (int k = 3; k <= sqrt_p_size; k += 2) { // Iterate to sqrt(n)
+        // We only want to find primes less than sqrt(N)
+        // We stride by 2*k because odd + odd = even
+        // And odd + even = odd, thereby skipping all even multiples
+        // All primes are odd aside from 2
+        #pragma omp parallel for
+        for (int prime = k*k; prime <= sqrt_p_size; prime += 2*k) {
+            mark[prime/2] = 1;
+            ++count;
+        }
+        
+        // Get next odd prime (unmarked value)
+        while (mark[k/2]) k += 2; 
+   }
+
+
+   /*number of primes*/
+   // Count starts from 1 to include 2
+   // i starts from 3 so that we only count odds
+   printf("There are %ld primes for sqrt(N).\n", count);
+   int* primes = (int *) malloc(count * sizeof(int));
+   primes[0] = 2; // Our algorithm doesn't include 2 because it's even
+
+   count = 1;
+   for(i = 3; i <= sqrt_p_size; i+=2){
+        if(mark[i/2] == 0) {
+        	printf("\t prime %ld\n", i );
+            primes
+        }
+   }
+
+   
+   /* end of preamble */
+
+   free(primes);
+
+   /* stop timer */
+   stop_timer();
+   time=elapsed_time ();
+
+   /*number of primes*/
+   // Count and i are magic numbers
+   count = 1;
+   for(i = 2; i <= N; i+=1){
+        if(mark[i] == 0) {
+        	//  printf("\t prime %ld  \n",i );
+        	++count;
+        }
+
+   }
+   printf("There are %ld primes less than or equal to %ld\n", count, problem_size);
+   /* print results */
+   printf("First three primes:");
+   j = 1;
+   printf("%d ", 2);
+   for ( i=3 ; i <= problem_size && j < 10; i+=2 ) {
+      if (mark[i/2]==0){
+            printf("%ld ", i);
+            ++j;
+      }
+   }
+   printf("\n");
+
+   printf("Last three primes:");
+   j = 0;
+   n=(problem_size%2?problem_size:problem_size-1);
+   for (i = n; i > 1 && j < 3; i-=2){
+     if (mark[i/2]==0){
+        printf("%ld ", i);
+        j++;
+     }
+   }
+   printf("\n");
+
+
+   printf("elapsed time = %lf (sec)\n", time);
+
+   free(mark);
+   return 0;
+}
+
+
