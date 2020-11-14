0ab4b11af66c45df88f1e4d9d78f4b19fd92a25b
Casey Key <caseykey@rams.colostate.edu>
2020-11-14 13:45:23 -0700

    reimplemented changes such as identity matrix and defining S as variable, but identity matrix isnt working

--- assignments/5/MMScanKernel.cu
+++ assignments/5/MMScanKernel.cu
@@ -3,6 +3,7 @@
 // Find N/G breakpoints for our scan
 // G groups/blocks
 __global__ void MMScanKernel00(float* X_GPU, float* R1_GPU, long N, long B){ 
+	long threadsPerBlock = (long) S;
   extern __shared__ float array[];
 	
 	// B0, B1 will hold intermediate products
@@ -20,32 +21,36 @@ __global__ void MMScanKernel00(float* X_GPU, float* R1_GPU, long N, long B){
 	long result;
 
 	memset(B0, 0, B * B * sizeof(float));
-	B0[0] = 1;
-  B1[0] = 1;
-	B2[0] = 1;
-	for (long i = 0; i < B; i++)
-		B0[i * B + i] = 1;
+	for (Y = 0; Y < B; Y++) {
+		B0[Y * B + Y] = 1;
+	}
 	__syncthreads();
 
   for (long Q = 0; Q < matricesPerBlock; Q++) {
     long currentMatrix = Q * elementsPerMatrix;
     long chain_offset = matrixOffset * elementsPerMatrix + currentMatrix;
-		for (Y = 0; Y < B/S; Y++) {
-      Y_offset = Y * B * S + threadIdx.y * B;
-      for (X = 0; X < B/S; X++) {
-      	X_offset = X * S + threadIdx.x;
+
+		for (Y = 0; Y < B/threadsPerBlock; Y++) {
+      Y_offset = Y * B * threadsPerBlock + threadIdx.y * B;
+
+      for (X = 0; X < B/threadsPerBlock; X++) {
+      	X_offset = X * threadsPerBlock + threadIdx.x;
+
         B1[Y_offset + X_offset] = X_GPU[chain_offset + Y_offset + X_offset];
       }
     }
 		
-		for (Y = 0; Y < B/S; Y++) {
-			Y_offset = Y * B * S + threadIdx.y * B;		
-			for (X = 0; X < B/S; X++) {
-				X_offset = X * S + threadIdx.x;
+		for (Y = 0; Y < B/threadsPerBlock; Y++) {
+			Y_offset = Y * B * threadsPerBlock + threadIdx.y * B;		
+
+			for (X = 0; X < B/threadsPerBlock; X++) {
+				X_offset = X * threadsPerBlock + threadIdx.x;
 				result=0;
+
 				for (P=0; P < B; P++) {
 					result += B0[Y_offset + P] * B1[X_offset + P * B];
 				}			
+
 				B2[X_offset + Y_offset] = result;
 			}
 		}
